8e49341e95b484fa2c0ff4e5f5dc1b7d6c19c6d8
mike <kmikeru@mail.ru>
2022-06-18 12:38:35 +0700

    TCP socket fix

--- lib/TCPInterface.dart
+++ lib/TCPInterface.dart
@@ -6,41 +6,47 @@ import 'package:meshtastic_proto/util.dart';
 
 class TCPInterface extends StreamInterface {
   final String hostname;
-  late Socket socket;
+  Socket? socket;
   TCPInterface({required this.hostname});
 
   @override
-  void connect() async {
-    socket = await Socket.connect(hostname, 4403);
-    print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
-    socket.listen(
-      (d) {
-        debug(formatDate(DateTime.now()) + ' TCP: ' + d.map((e) => e.toRadixString(16).padLeft(2, '0')).join(' '));
-        super.reader(d);
-      },
-      // handle errors
-      onError: (error) {
-        print(error);
-        socket.destroy();
-      },
-      // handle server ending connection
-      onDone: () {
-        print('Server left.');
-        socket.destroy();
-      },
-    );
-    super.startConfig();
+  void connect() {
+    Socket.connect(hostname, 4403).then((sock) {
+      this.socket = sock;
+      print('Connected to: ${sock.remoteAddress.address}:${sock.remotePort}');
+      sock.listen(
+        (d) {
+          debug(formatDate(DateTime.now()) + ' TCP: ' + d.map((e) => e.toRadixString(16).padLeft(2, '0')).join(' '));
+          super.reader(d);
+        },
+        // handle errors
+        onError: (error) {
+          print('socket.onError:' + error.toString());
+          this.socket = null;
+          sock.destroy();
+        },
+        // handle server ending connection
+        onDone: () {
+          print('Server left.');
+          this.socket = null;
+          sock.destroy();
+        },
+      );
+      super.startConfig();
+    }).catchError((e) {
+      print('catcherror:' + e.toString());
+    });
   }
 
   @override
   void close() {
-    socket.destroy();
+    socket?.destroy();
     super.close();
   }
 
   @override
   void writeBytes(Uint8List b) async {
-    socket.add(b);
-    await socket.flush();
+    socket?.add(b);
+    await socket?.flush();
   }
 }
